059d0ff79295350d05ba5cf4595f811638227316
Ivelin <i.todorov153@gmail.com>
2017-01-18 21:18:12 +0200

    Some of my codes.
    A solved problem from my SoftUni entery exam.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pokemonsFirstPlayer = int.Parse(Console.ReadLine());
+            var pokemonsSecondPlayer = int.Parse(Console.ReadLine());
+            var maxBattles = int.Parse(Console.ReadLine());
+            var counter = 0;
+
+            for (int i = 1; i <= pokemonsFirstPlayer; i++)
+            {
+                for (int j = 1; j <= pokemonsSecondPlayer; j++)
+                {
+                    if (counter >= maxBattles)
+                    {
+                        break;
+                    }
+                    Console.Write("({0} <-> {1}) ",
+                        i,
+                        j);
+                    counter++;                    
+                }
+            }
+
+        }
+    }
+}
