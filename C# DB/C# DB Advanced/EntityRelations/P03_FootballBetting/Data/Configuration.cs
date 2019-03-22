011617e382bf8657994dc5d9392af042f2168a9a
Ivelin <i.todorov153@gmail.com>
2019-03-22 13:28:27 +0200

    Delete Configuration.cs

--- C# DB/C# DB Advanced/EntityRelations/P03_FootballBetting/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P03_FootballBetting.Data
-{
-    public class Configuration
-    {
-        public const string connectionString = "Server=.;Database=FootballBetting;Integrated Security = True;";
-    }
-}
