e84d75dc61f3626b6e23a3d81046950f1b97807a
Ivelin <i.todorov153@gmail.com>
2019-03-22 13:27:02 +0200

    Delete Configuration.cs

--- C# DB/C# DB Advanced/BillsPaymentSystem/BillsPaymentSystem.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BillsPaymentSystem.Data
-{
-    public class Configuration
-    {
-        public static string ConnectionString => "Server=.;Database=BillsPaymentSystem;Integrated Security=True";
-    }
-}
