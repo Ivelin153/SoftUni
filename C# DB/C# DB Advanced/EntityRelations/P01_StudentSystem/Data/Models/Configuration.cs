14a23bbd0d4e973da95d962ec4d18756dfd42362
Ivelin <i.todorov153@gmail.com>
2019-03-22 13:28:13 +0200

    Delete Configuration.cs

--- C# DB/C# DB Advanced/EntityRelations/P01_StudentSystem/Data/Models/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P01_StudentSystem.Data.Models
-{
-    public class Configuration
-    {
-        public const string connectionString = "Server=.;Database=StudentSystem;Integrated Security = True;";
-    }
-}
